121bfd0caaf7ab0f26753ac13a6195164c1e3345
Pablo Assis <147431577+assispablo@users.noreply.github.com>
2024-10-18 17:38:23 -0300

    Add files via upload

---
+++ Exchange.java
@@ -0,0 +1,20 @@
+public class Exchange {
+    private final double cotacaoDolar = 5.5;
+    private final double iof = 0.0586;
+    private final double taxaOperacaoEmPercentual = 1;
+
+    @SuppressWarnings("unused")
+    public double comprarDolar(double valorReal) {
+        double valorDolar = valorReal / cotacaoDolar;
+        double valorDolarComIOF = valorDolar - (valorDolar * iof);
+        double taxaOperacao = valorDolar * taxaOperacaoEmPercentual;
+        return valorDolarComIOF;
+    }
+
+    public static void main(String[] args) {
+        Exchange exchange = new Exchange();  //alteração de no exchange, trocando a letra maiuscula por minuscula
+        double valorReal = 1000;
+        double valorDolar = exchange.comprarDolar(valorReal);
+        System.out.println("Valor em dólar: " + valorDolar);
+    }
+}
---
+++ Looping.java
@@ -0,0 +1,10 @@
+public class Looping {
+    public static void main(String[] args) {
+        int x = 0;
+        for ( int i = 0; i < 10; i++) {
+            x = i;
+        }
+    System.out.println(x);
+    }
+    
+}
